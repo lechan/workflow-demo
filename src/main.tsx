9f1e9c44a93528f9ba37acc99d5711bfe18503ec
lechan <lechan@126.com>
2025-05-27 10:51:20 +0800

    fix: 处理antd中文的展示

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,14 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import zhCN from 'antd/es/locale/zh_CN';
+import { ConfigProvider } from 'antd';
+import 'antd/dist/antd.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ConfigProvider locale={zhCN}>
+      <App />
+    </ConfigProvider>
   </StrictMode>,
 )
